78d5c7a2f2b098f721f41bb315a4d7515a195341
chris <maxlongbow@gmail.com>
2022-07-07 12:32:41 +0200

    update

---
+++ ONT_snp_calling.sh
@@ -0,0 +1,19 @@
+# 7 july 2022
+
+# Tutorial uses minimap2 to bam file, and the Sniffles to VCF
+https://timkahlke.github.io/LongRead_tutorials/VC_S.html
+
+# ONT data mapped to P. brassicae genome for SNP calling and CRISPR cut site eval
+
+
+# mapping using minimap2
+/data/programs/minimap2/minimap2
+
+
+
+# variant calling using SNIFFLES
+https://github.com/fritzsedlazeck/Sniffles
+# install locally
+pip install sniffles
+# then from terminal
+sniffles
---
+++ freebayes_parallel_KT.sh
@@ -0,0 +1,8 @@
+polished it a bit further, hopefully it is straight forward.
+
+If you want to run it on duke you willl need to download both scripts and change the path to goleft and the modified freebayes parallel script.
+
+https://github.com/kallekarlhugo/useful_scripts
+
+
+# se
